a89b67d0b9099cbbb8008c5dec29f2adabefc5fb
unknown <dakshrana2024@gmail.com>
2026-06-09 12:20:52 +0530

    Allow all CORS origins in server/index.js for production deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import newsletterRouter from './routes/newsletter.js';
 const app = express();
 const PORT = 5000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productsRouter);
